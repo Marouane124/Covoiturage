c8c9a4fd5b61eab39f5c6b3228371b4e1ec5110e
khawla01 <khawla.aafif02@gmail.com>
2024-11-11 23:05:24 +0100

    ajout de la partie web

---
+++ FrontEnd/web/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    mapboxToken: 'pk.eyJ1Ijoic2ltb2FpdGVsZ2F6emFyIiwiYSI6ImNtMzZiMmM0ZzAzcGkyanNlb3BjY2Q4aG0ifQ.0Zo7nzPFESCdx22sNjfWmA'
+  };
+  
